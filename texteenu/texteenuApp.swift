51f1c3b594c98549e92dae62947219a06d62ba98
Gonzalo <gonzalominuto@gmail.com>
2026-04-12 19:01:35 -0400

    Initial Commit

---
+++ texteenu/texteenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  texteenuApp.swift
+//  texteenu
+//
+//  Created by Gonzalo Minuto on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct texteenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
